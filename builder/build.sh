3ef0338ce25f52d1708f2fc92dc8a0f3e524f811
Levi Yan <leviyanx@gmail.com>
2022-10-17 18:06:28 +0800

    refactor(builder): remove ubuntu layer and access to fission successfully

--- builder/build.sh
+++ builder/build.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
-set -euxo pipefail
-
-srcDir=/usr/src/$(basename ${SRC_PKG})
-
-if [ -d ${SRC_PKG} ]; then
-    echo "Building in directory ${scrDir}"
-    ln -sf ${SRC_PKG} ${srcDir}
-elif [ -f ${SRC_PKG} ]; then
-    # only file
-    echo "Building file ${SRC_PKG} in ${srcDir}"
-    mkdir -p ${srcDir}
-    cp ${SRC_PKG} ${srcDir}/function.rs
-fi
-
-cd ${srcDir}
-
-# function
-rustc +nightly --target wasm32-unknown-unknown -O function.rs
-
-cp function.wasm ${DEPLOY_PKG}
+# cd ${SRC_PKG}
+echo "start building"
+cargo build --target wasm32-wasi
+cp ${SRC_PKG}/target/wasm32-wasi/debug/*.wasm ${DEPLOY_PKG}
+echo "finish building"
