7c514a1aaf46276f1a54735b777c56618b30aa91
Levi Yan <leviyanx@gmail.com>
2022-10-18 17:56:23 +0800

    fix(builder): finish building process successfully

--- builder/build.sh
+++ builder/build.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-# cd ${SRC_PKG}
 echo "start building"
+
+# check dir
+echo "src path: ${SRC_PKG}"
+pwd
+ls -l
+
+# if exist "__MACOSX" (archive with macos), remove it; else don't need to this step
+if [ -d "__MACOSX"]; then
+    rm -rf __MACOSX
+fi
+
+# enter source dir``
+cd */ || cd hello
+# check source dir
+ls -l
+
+# print file in src
+ls src
+
+# build
 cargo build --target wasm32-wasi
-cp ${SRC_PKG}/target/wasm32-wasi/debug/*.wasm ${DEPLOY_PKG}
+# print build result
+ls -l
+ls target/wasm32-wasi/debug/
+
+cp ./target/wasm32-wasi/debug/*.wasm ${DEPLOY_PKG}
+
 echo "finish building"
